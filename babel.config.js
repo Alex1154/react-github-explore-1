0986d22f1f6af034a50f67a6daf167bd644cf035
João Alexandre <aleramosfilho@gmail.com>
2021-03-22 18:05:29 -0300

    feature

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,13 @@
-const { runtime } = require("webpack");
+const { runtime } = require('webpack');
 
 module.exports = {
   presets: [
-    "@babel/preset-env",
+    '@babel/preset-env',
+    '@babel/preset-typescript',
     [
-      "@babel/preset-react",
+      '@babel/preset-react',
       {
-        runtime: "automatic",
+        runtime: 'automatic',
       },
     ],
   ],

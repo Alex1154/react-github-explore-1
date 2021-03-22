6ee25952ffa77875534a383eb257b4bc44f074c3
João Alexandre <aleramosfilho@gmail.com>
2021-03-22 18:02:18 -0300

    react-github-explore feature

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+const { runtime } = require("webpack");
+
+module.exports = {
+  presets: [
+    "@babel/preset-env",
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
+  ],
+};
